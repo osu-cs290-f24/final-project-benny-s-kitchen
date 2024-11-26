360e256971d6e00e48fd6312594d05d308d521bb
Vi Vuong <vuongv@oregonstate.edu>
2024-11-26 12:52:19 -0800

    Added recipe model, controller and routes

---
+++ models/recipeModel.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const recipeSchema = new mongoose.Schema({
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    ingredients: { type: [String], required: true },
+    instructions: { type: [String], required: true },
+    images: { type: [String] }, // URLs or file paths
+    categoryTags: { type: [String] },
+    preparationTime: { type: Number, required: true }, // In minutes
+    difficulty: { type: String, enum: ['Easy', 'Medium', 'Hard'], required: true }
+}, { timestamps: true });
+
+const Recipe = mongoose.model('Recipe', recipeSchema);
+
+module.exports = Recipe;
