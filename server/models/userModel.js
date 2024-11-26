a943d10082683c797ec91e60f42957d9cef6e402
Vi Vuong <vuongv@oregonstate.edu>
2024-11-26 14:26:54 -0800

    Remove id field in user schema

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-  id: { type: String, required: true },
   username: { type: String, required: true },
   email: { type: String, required: true },
   hashedPassword: { type: String, required: true },
